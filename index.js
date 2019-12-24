5b73fb5f008d9e7026293f369e46634a06854744
hhhejiaman <1258394564@qq.com>
2019-12-24 20:44:14 +0800

    [he]{feat}:add localStorage & line-through checkbox

--- index.js
+++ index.js
@@ -1,8 +1,56 @@
 window.onload = function() {
     var userInfo = document.getElementById("user-info");
-    var addInfo = document.getElementById("add-info");
+    var addBtn = document.getElementById("add-info");
     var addList = document.getElementById("add-list");
-    addInfo.onclick = function() {
+    var html = "";
+    userInfo.addEventListener("keydown", function(event) {
+        if (event.keyCode == 13) {
+            listJoin(event);
+
+        }
+    });
+    addBtn.addEventListener("click", function(event) {
+        listJoin(event);
+
+    })
+
+    function listJoin(event) {
+        if (userInfo.value != "") {
+            var storage = window.localStorage;
+            var index = storage.length;
+            var listThing = {
+                "index": index,
+                "content": userInfo.value,
+                "hasCheck": false
+            }
+            storage.setItem(index, JSON.stringify(listThing));
+            console.log(storage)
+            userInfo.value = "";
+            html += `<li><input type="checkbox" class="list-check" check=${listThing.hasCheck}>${listThing.content}</li> `
+            addList.innerHTML = html;
+        }
+
+    };
+
+    addList.addEventListener("click", function() {
+        var liNow = document.getElementsByTagName("li");
+        var listCheck = document.getElementsByClassName("list-check");
+        console.log(listCheck)
+        for (i = 0; i < liNow.length; i++) {
+            if (listCheck[i].checked) {
+                listCheck[i].parentNode.style.textDecorationLine = "line-through";
+                listCheck[i].parentNode.style.color = "grey";
+            }
+        }
+    });
+
+
+
+
+
+
+    /*addInfo.onclick = function() {
+
         if (userInfo.value != "") {
             var li = document.createElement("li");
             li.setAttribute("class", "list-row");
@@ -14,13 +62,7 @@ window.onload = function() {
         } else {
             return false;
         }
-    }
 
-    var listCheck = document.getElementsByClassName("list-check");
-    console.log(listCheck)
-    var allBtn = document.getElementById("all-btn");
-    this.console.log(allBtn)
-    allBtn.onclick = function() {
         alert("1");
-    }
+    }*/
 }
